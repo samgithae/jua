98d3e909a2510284cab8ad1c4c746953dd4053a3
Samuel Githae <samgithae01@gmail.com>
2017-06-29 11:58:50 +0300

    added withdraw savings

--- views/includes/register_employee.inc.php
+++ views/includes/register_employee.inc.php
@@ -7,33 +7,34 @@
  */
 
 use Hudutech\Services\FileUploader;
+
 $successMsg = '';
 $errorMsg = '';
-if(isset($_POST['first_name'], $_POST['middle_name'], $_POST['last_name'])) {
+if (isset($_POST['first_name'], $_POST['middle_name'], $_POST['last_name'])) {
     $employee = new \Hudutech\Entity\Employee();
     $fullName = $_POST['first_name'] . " " . $_POST['middle_name'] . " " . $_POST['last_name'];
-    $employee-> setPfNo($_POST['pf_no']);
-    $employee-> setFullName($fullName);
-    $employee-> setJobTitle($_POST['job_title']);
-    $employee-> setIdNo($_POST['id_no']);
-    $employee-> setKraPin($_POST['kra_pin']);
-    $employee-> setNssfNo($_POST['nssf_no']);
-    $employee-> setNhifNo($_POST['nhif_no']);
-    $employee-> setRemuneration($_POST['remuneration']);
-    $employee-> setJobDescription($_POST['job_description']);
-    $employee-> setQualification($_POST['qualification']);
-    $employee-> setTestimonial($_POST['testimonial']);
-    $employee-> setBankName($_POST['bank_name']);
-    $employee-> setBankAccountNo($_POST['bank_account_no']);
-    $employee-> setPostalAddress($_POST['postal_address']);
-    $employee-> setEmail($_POST['email']);
-    $employee-> setPhoneNumber($_POST['phone_number']);
-    $employee-> setNokName($_POST['nok_name']);
-    $employee-> setNokRelationship($_POST['nok_relationship']);
-    $employee-> setNokContact($_POST['nok_contact']);
+    $employee->setPfNo($_POST['pf_no']);
+    $employee->setFullName($fullName);
+    $employee->setJobTitle($_POST['job_title']);
+    $employee->setIdNo($_POST['id_no']);
+    $employee->setKraPin($_POST['kra_pin']);
+    $employee->setNssfNo($_POST['nssf_no']);
+    $employee->setNhifNo($_POST['nhif_no']);
+    $employee->setRemuneration($_POST['remuneration']);
+    $employee->setJobDescription($_POST['job_description']);
+    $employee->setQualification($_POST['qualification']);
+    $employee->setTestimonial($_POST['testimonial']);
+    $employee->setBankName($_POST['bank_name']);
+    $employee->setBankAccountNo($_POST['bank_account_no']);
+    $employee->setPostalAddress($_POST['postal_address']);
+    $employee->setEmail($_POST['email']);
+    $employee->setPhoneNumber($_POST['phone_number']);
+    $employee->setNokName($_POST['nok_name']);
+    $employee->setNokRelationship($_POST['nok_relationship']);
+    $employee->setNokContact($_POST['nok_contact']);
     $employee->setDateOfHire($_POST['dateOfHire']);
 
-    if(isset($_FILES['passport'])){
+    if (isset($_FILES['passport'])) {
         $uploader = new FileUploader('image');
         $target_dir = 'uploads/passports/';
         // $target_dir = 'uploads/';
@@ -42,19 +43,19 @@ if(isset($_POST['first_name'], $_POST['middle_name'], $_POST['last_name'])) {
 //name of the form input
         $form_name = 'passport';
         $fileUploaded = $uploader->uploadFile($target_dir, $form_name);
-        if($fileUploaded){
+        if ($fileUploaded) {
 //retrieve the file path
             $file_url = $uploader->getFilePath();
 //use the file_url to set the value for db column
             $employee->setPassport($file_url);
 
         }
-    }else{
+    } else {
         //$client->setPassport(null);
         $errorMsg .= "passport not uploaded";
     }
 
-    if(isset($_FILES['idAttachment'])){
+    if (isset($_FILES['idAttachment'])) {
         $uploader = new FileUploader('image');
         $target_dir = 'uploads/ids/';
         // $target_dir = 'uploads/';
@@ -63,43 +64,34 @@ if(isset($_POST['first_name'], $_POST['middle_name'], $_POST['last_name'])) {
 //name of the form input
         $form_name = 'idAttachment';
         $fileUploaded = $uploader->uploadFile($target_dir, $form_name);
-        if($fileUploaded){
+        if ($fileUploaded) {
 //retrive the file path
             $file_url = $uploader->getFilePath();
 //use the file_url to set the value for db column
             $employee->setIdAttachment($file_url);
 
         }
-    }else{
+    } else {
         //$client->setPassport(null);
         $errorMsg .= "Id attachment not uploaded";
     }
 
 
+    $employeeControl = new \Hudutech\Controller\EmployeeController();
+    $created = $employeeControl->create($employee);
 
 
-    $employeeControl= new \Hudutech\Controller\EmployeeController();
-    $created= $employeeControl->create($employee);
-
-
-    if ($created===true) {
+    if ($created === true) {
         $successMsg .= "Client details saved successfully";
-    } elseif(array_key_exists('error',$created)) {
+    } elseif (array_key_exists('error', $created)) {
         $errorMsg .= "{$created['error']}";
 
     }
 
 
-
-
-
-
-
-}
-else{
+} else {
     $errorMsg .= "KEY  FIELDS REQUIERED";
 }
-
 
 
 ?>
---
+++ views/savingWithdraw.php
@@ -0,0 +1,172 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: New LAptop
+ * Date: 04/05/2017
+ * Time: 00:42
+ */
+require_once __DIR__.'/../vendor/autoload.php';
+
+$clients = \Hudutech\Controller\ClientController::all();
+$loans= \Hudutech\Controller\LoanController::all();
+
+include  __DIR__.'/includes/lead_loan.inc.php';
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Withdraw Savings</title>
+
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+
+
+    <!-- Bootstrap Core CSS -->
+    <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- MetisMenu CSS -->
+    <link href="../vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="../dist/css/sb-admin-2.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="../vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+<div id="wrapper">
+    <?php include_once 'right_menu.php'?>
+    <div id="page-wrapper">
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">Saving Withdraw</h1>
+            </div>
+            <!-- /.col-lg-12 -->
+        </div>
+        <!-- /.row -->
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        Withdraw cash
+
+
+
+                    </div>
+                    <div class="panel-body">
+                        <div class="row">
+
+                            <div class="col-lg-6 col-md-offset-3">
+
+
+                                <?php
+                                if(empty($success_msg) && !empty($error_msg)){
+                                    ?>
+                                    <div class="alert alert-danger alert-dismissable">
+                                        <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                                        <?php echo $error_msg ?>
+                                    </div>
+                                    <?php
+                                }
+                                elseif(empty($error_msg) and !empty($success_msg)){
+                                    ?>
+                                    <div class="alert alert-success alert-dismissable">
+                                        <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                                        <?php echo $success_msg  ?>
+                                    </div>
+
+                                    <?php
+                                }
+                                else
+                                {
+                                    echo "";
+                                }
+                                ?>
+
+                                <form class="form-horizontal" method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF'])?>">
+
+                                    <div class="form-group">
+                                        <label for="name" class="cols-sm-2 control-label">Client Name</label>
+                                        <div class="cols-sm-10">
+                                            <div class="input-group">
+                                                <span class="input-group-addon"><i class="fa fa-user fa" aria-hidden="true"></i></span>
+                                                <select name="clientId" class="form-control">
+                                                    <option>--Select Client here--</option>
+                                                    <?php foreach ($clients as $client): ?>
+                                                        <option value="<?php echo $client['id']?>"><?php echo $client['fullName']?></option>
+                                                    <?php endforeach ?>
+
+                                                </select>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="email" class="cols-sm-2 control-label">Loan Type</label>
+                                        <div class="cols-sm-10">
+                                            <div class="input-group">
+                                                <span class="input-group-addon"><i class="fa fa-cog fa" aria-hidden="true"></i></span>
+                                                <select name="loanId" class="form-control">
+                                                    <option>--Select Loan type--</option>
+                                                    <?php foreach ($loans as $loan): ?>
+                                                        <option value="<?php echo $loan['id']?>"><?php echo $loan['loanType']?></option>
+                                                    <?php endforeach ?>
+
+                                                </select>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="username" class="cols-sm-2 control-label">Amount</label>
+                                        <div class="cols-sm-10">
+                                            <div class="input-group">
+                                                <span class="input-group-addon"><i class="fa fa-money fa" aria-hidden="true"></i></span>
+                                                <input type="number" class="form-control" name="amount"   placeholder="Loan amount" />
+
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group ">
+                                        <input type="submit" name="submit" value="Save" class="btn btn-primary btn-lg btn-block login-button">
+                                    </div>
+                                </form>
+
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include_once 'footer.php'?>
+</body>
+</html>
